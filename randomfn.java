7330c250eb2a36b0485f11420a7601f04b4d3ff9
dk432664 <43929505+dk432664@users.noreply.github.com>
2018-10-08 14:17:29 +0530

    randomfn.java

---
+++ randomfn.java
@@ -0,0 +1,16 @@
+//random function
+public class ass1 {
+	public static void main(String[] args)
+	{
+		int m=-5+8*6;
+		int n=(55+9)%9;
+		int o=20-3*5/8;
+		int p=5+15/3*2-8%3;
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(o);
+		System.out.println(p);
+		
+	}
+
+}
