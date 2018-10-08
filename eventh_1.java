9f13c8997229bf389d8dfe6613bfb12316d49f06
Genius <36341114+deepamgupta@users.noreply.github.com>
2018-10-08 19:51:44 +0530

    Eventh
    Hey add this file

---
+++ eventh_1.java
@@ -0,0 +1,14 @@
+# skippy
+//eventhandling1
+Import javax.swing.JOptionPane;
+Public class deep{
+Public static void main(String[] args)
+{
+String a=JOptionPane.showInputDialog(“enter fn”);
+String b=JOptionPane.showInputDialog(“enter sn”);
+Int x=Integer.Parse.Int(a);
+Int y=Integer.Parse.Int(b);
+Int s=x+y;
+JOptionPane.showMessageDialog(null,”the ans is “ +sum,”the title is”,JOptionPane.PLAIN_MESSAGE);
+}
+}
